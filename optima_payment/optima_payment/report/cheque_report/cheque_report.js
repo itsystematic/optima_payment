d097b84191176648f3474e73ef9ea125a7141167
yasser1248 <yasserelbana@gmail.com>
2024-10-17 12:29:58 +0300

    Fix: label in JS to Enhance Translatoin

--- optima_payment/optima_payment/report/cheque_report/cheque_report.js
+++ optima_payment/optima_payment/report/cheque_report/cheque_report.js
@@ -74,10 +74,39 @@ frappe.query_reports["Cheque Report"] = {
 		{
 			fieldname: "party",
 			label: __("Party"),
-			fieldtype: "Dynamic Link",
-			options: "party_type"	,
-			depends_on : "party_type"
+			fieldtype: "MultiSelectList",
+			// options: "party_type"	,
+			depends_on : "party_type",
+			get_data: function (txt) {
+				doc = frappe.query_report.get_filter_value("party_type");
+				return frappe.db.get_link_options(doc, txt, {
+					// company: frappe.query_report.get_filter_value("company"),
+				});
+			},
 		},
+		{
+			fieldname: "names",
+			label: __("Vourcher No"),
+			fieldtype: "MultiSelectList",
+			options: "Payment Entry" ,
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Payment Entry", txt, {
+					"cheque_status" : ["in" , [
+						"For Collection" ,
+						"Collected" ,
+						"Returned" ,
+						"Return To Holder" , 
+						"Deposit Under Collection" , 
+						"Endorsed" ,
+						"Rejected" , 
+						"Cancelled" ,  
+						"Encashment",
+						"Issuance" ,
+						"Issuance From Endorsed"
+					]]
+				})
+			}
+		}
 
 
 	],
