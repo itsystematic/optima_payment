ba499417eb3eca7a5ff8491f3c919b59244e8c99
yasser1248 <yasserelbana@gmail.com>
2025-10-29 13:59:55 +0300

    refactor: after install hooks to point to specific function seperated from setup

--- optima_payment/hooks.py
+++ optima_payment/hooks.py
@@ -102,7 +102,7 @@ jinja = {
 # ------------
 
 # before_install = "optima_payment.install.before_install"
-# after_install = "optima_payment.install.after_install"
+after_install = "optima_payment.install.after_install"
 
 after_migrate = "optima_payment.migrate.after_migrate"
 
