4d9548ad789b01e10af803cca470edfa3109cbbe
FawazAlhafiz <fawaztwix111@gmail.com>
2026-06-07 16:18:11 +0300

    refactor(cheque-action-log): remove commented-out code for add_cheque_action_log function

--- optima_payment/optima_payment/doctype/cheque_action_log/cheque_action_log.py
+++ optima_payment/optima_payment/doctype/cheque_action_log/cheque_action_log.py
@@ -57,29 +57,3 @@ def add_cheque_action_log(doc , cheque_status , mode_of_payment=None , bank_fees
 
     doc.db_set(fields_updated)
 
-
-
-# @frappe.whitelist()
-# def add_cheque_action_log(cheque_action_details):
-#     """Add a log entry for a cheque action using details from a dictionary."""
-#     doc = cheque_action_details["doc"]
-#     # cheque_status = cheque_action_details["cheque_status"]
-#     # mode_of_payment = cheque_action_details.get("mode_of_payment")
-#     # bank_fees_amount = cheque_action_details.get("bank_fees_amount", 0.00)
-
-#     frappe.get_doc({
-#         "doctype": "Cheque Action Log",
-#         "payment_entry": doc.name,
-#         "cheque_status": cheque_action_details.get("cheque_status"),
-#         "mode_of_payment": cheque_action_details.get("mode_of_payment"),
-#         "bank_fees_amount": cheque_action_details.get("bank_fees_amount", 0.00)
-#     }).insert(ignore_mandatory=True, ignore_permissions=True, ignore_links=True)
-
-#     fields_updated = {"cheque_status": cheque_status}
-
-#     if mode_of_payment and doc.docstatus == 1:
-#         fields_updated["pay_mode_of_payment"] = mode_of_payment
-#     if bank_fees_amount and doc.docstatus == 1:
-#         fields_updated["bank_fees_amount"] = bank_fees_amount
-
-#     doc.db_set(fields_updated)
