917eed1083eb856c1cee34dc8d7958b9d241ac1c
FawazAlhafiz <fawaztwix111@gmail.com>
2026-06-25 15:22:11 +0300

    feat(letter-of-credit): add no_copy attribute to lc_percent field and remove lc_mode_of_payment field

--- optima_payment/optima_payment/doctype/optima_payment_setting/optima_payment_setting.py
+++ optima_payment/optima_payment/doctype/optima_payment_setting/optima_payment_setting.py
@@ -7,6 +7,29 @@ from frappe import get_app_path
 
 
 class OptimaPaymentSetting(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+        from optima_payment.optima_payment.doctype.cheque_accounts.cheque_accounts import ChequeAccounts
+
+        bank_guarantee_bank_fees_account: DF.Link | None
+        bank_guarantee_insurance_account: DF.Link | None
+        bank_guarantee_loss_expense_account: DF.Link | None
+        bank_guarantee_receiving_insurance_account: DF.Link | None
+        cheque_accounts: DF.Table[ChequeAccounts]
+        company: DF.Link
+        enable_auto_deposit_under_collection_in_time: DF.Check
+        enable_auto_pay_cheque_in_time: DF.Check
+        enable_optima_payment: DF.Check
+        lc_bank_fees_account: DF.Link | None
+        lc_insurance_account: DF.Link | None
+        lc_loss_expense_account: DF.Link | None
+        lc_receiving_insurance_account: DF.Link | None
+    # end: auto-generated types
 
     def before_save(self):
         self.enable_print_format()
